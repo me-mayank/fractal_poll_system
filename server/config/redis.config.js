2d62dd06fde46b7eda4ba5128c5297696a3ae446
me-mayank <174928744+me-mayank@users.noreply.github.com>
2026-03-23 19:55:07 +0530

    fixed issues related to the redis configuration

--- server/config/redis.config.js
+++ server/config/redis.config.js
@@ -1,16 +1,15 @@
 import { createClient } from "redis";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const redis = createClient({
   url: process.env.REDIS_URL,
 });
 
 redis.on("error", (err) => console.log("Redis Error:", err));
 
-await redis.connect();
-
-console.log("Connected to Upstash Redis");
+export async function connectRedis() {
+  await redis.connect();
+  console.log("Connected to Upstash Redis");
+}
 
 export default redis;
